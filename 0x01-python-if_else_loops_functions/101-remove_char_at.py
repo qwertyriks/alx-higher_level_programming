c4b1ac4358eeb8ff8d01beb59bef9b88e04a9bb2
qwertyriks <romeokumatse@gmail.com>
2024-01-03 06:43:38 -0800

    fix on 101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    """This code will create a copy of the string without the character at position n."""
+    """This code will create a copy of the string
+        without the character at position n."""
     if n < 0:
         return (str)
     return (str[:n] + str[n+1:])

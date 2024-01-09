f250a3f4df28d072430ddeea10c5f920ed54263f
qwertyriks <romeokumatse@gmail.com>
2024-01-08 16:12:23 -0800

    Completely

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    # outputs the length of a string and its first character.
-    if sentence == "":
-        return (0, None)
-    return (len(sentence), sentence[0])i
+
+multiple_returns = __import__('8-multiple_returns').multiple_returns
+sentence = "At school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))

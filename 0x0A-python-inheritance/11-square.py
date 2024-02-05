ffbda1bdc7fad9e0594de2984c120afa6357d2b7
qwertyriks <romeokumatse@gmail.com>
2024-02-05 13:12:33 -0800

    python Inheritance.

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-
-"""This the defines square module"""
+"""This will represent the Rectangle module."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Now,the body of the Square class body"""
+    """This represent the class body."""
 
     def __init__(self, size):
-        super().integer_validator("size", size)
-        self.__size = size
-        Rectangle.__init__(self, size, size)
 
-    def area(self):
-        return self.__size * self.__size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size

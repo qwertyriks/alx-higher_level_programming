54e049517f726eea0166b62e0202ad148a21e74d
qwertyriks <romeokumatse@gmail.com>
2024-04-11 11:30:58 +0000

    It is well with my soul

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-    const newList = [];
-    while (list.length) {
-      newList.push(list.pop());
-    }
-    return newList;
-  };
+  const newList = [];
+  while (list.length) {
+    newList.push(list.pop());
+  }
+  return newList;
+};

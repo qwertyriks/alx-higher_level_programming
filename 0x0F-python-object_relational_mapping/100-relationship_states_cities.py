12ac7dbfc877ce4332a020b31876642c86dfe2e7
qwertyriks <romeokumatse@gmail.com>
2024-04-14 21:47:38 +0000

    Indentation checks

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -17,7 +17,10 @@ if __name__ == "__main__":
     """
 
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3])
+        argv[1],
+        argv[2],
+        argv[3]
+        )
     engine = create_engine(db_uri)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -16,8 +16,12 @@ if __name__ == '__main__':
     from the database.
     """
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1], port=3306,
+        passwd=argv[2],
+        db=argv[3]
+        )
 
     with db.cursor() as cur:
         cur.execute("""

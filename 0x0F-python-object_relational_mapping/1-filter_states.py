2a3f7c8fbb5cf0e85425d8aed6420140cb2808ee
qwertyriks <romeokumatse@gmail.com>
2024-04-14 14:29:39 -0700

    Indentation correction 0-2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,8 +13,13 @@ if __name__ == '__main__':
     Has access to the database and states
     from the database.
     """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(
+            host="localhost",
+            user=argv[1],
+            port=3306,
+            passwd=argv[2],
+            db=argv[3]
+            )
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states \

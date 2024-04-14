bfa7bb1f9476138efcf77abd109dc82444e13dd3
qwertyriks <romeokumatse@gmail.com>
2024-04-14 20:13:57 +0000

    0x0F

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Lists all State objects and corresponding
-City objects contained in the DB
+Lists all City objects from
+the database hbtn_0e_101_usa
 """
 import sys
 from relationship_state import Base, State
@@ -14,14 +14,12 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    st = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+    st = session.query(State).join(City).order_by(City.id).all()
 
     for state in st:
-        print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
+            print("{}: {} -> {}".format(city.id, city.name, state.name))
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Gonna lists all citie cities from database
+Gonna lists all cities of a state in argument
 """
 import MySQLdb
 import sys
@@ -10,10 +10,11 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     c = db.cursor()
-    c.execute("""SELECT cities.id, cities.name, states.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id""")
+    c.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
     rows = c.fetchall()
-    for row in rows:
-        print(row)
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
     c.close()
     db.close()

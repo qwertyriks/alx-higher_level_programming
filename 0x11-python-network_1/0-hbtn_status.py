dbbbd6608528bfd1728eb0906127151728b80be1
qwertyriks <romeokumatse@gmail.com>
2024-04-26 12:29:37 -0700

    In progress

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,3 +10,4 @@ if __name__ == "__main__":
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
+

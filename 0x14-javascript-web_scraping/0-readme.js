77ba96036f28543d555ba5be7a2498c94317bb7d
qwertyriks <romeokumatse@gmail.com>
2024-05-24 02:06:30 +0000

    Push harder

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-// Importing the built-in Node.js 'fs' module...
-
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
   // Use fs.readFile() to read the contents of a file specified
   // as a command-line argument

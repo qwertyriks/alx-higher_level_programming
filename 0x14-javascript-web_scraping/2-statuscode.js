90ba5948b4b429303f512d9ce4f997fd3f209aaf
qwertyriks <romeokumatse@gmail.com>
2024-05-24 03:42:53 -0700

    Task Complete

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,18 +3,3 @@ const request = require('request');
 request.get(process.argv[2]).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
 });
-=======
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
-  }
-});
-

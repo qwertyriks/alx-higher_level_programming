ff6df2f1ce35956995e20e0f194d35fc94ea9b66
qwertyriks <romeokumatse@gmail.com>
2024-05-24 02:09:06 +0000

    Harder

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  // Use fs.readFile() to read the contents of a file specified
-  // as a command-line argument
-  // The 'utf8' specifies the encoding of the file being read
-
-  if (error) {
-    // Incase an error occurs during the file read operation,
-    // the 'error' parameter will contain an error object.
-    console.error('Error reading the file:', error);
-
-  } else {
-    // Assuming the file is read successfully, the 'content' parameter
-    // will contain the contents of the file as a string.
-    console.log(content);
-  }
+  console.log(error || content);
 });
